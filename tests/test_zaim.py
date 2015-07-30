03c8c73df6c9fc63718cf50353e6bdf330c4d59f
Hiromu <hiromu2000@hotmail.com>
2015-07-30 21:43:09 +0900

    Supported Python 3

--- tests/test_zaim.py
+++ tests/test_zaim.py
@@ -5,10 +5,10 @@ from zaim import Zaim
 
 class TestZaim(unittest.TestCase):
     def setUp(self):
-        consumer_key = unicode(os.environ.get("ZAIM_CONSUMER_KEY", ""))
-        consumer_secret = unicode(os.environ.get("ZAIM_CONSUMER_SECRET", ""))
-        access_token = unicode(os.environ.get("ZAIM_ACCESS_TOKEN", ""))
-        access_token_secret = unicode(os.environ.get("ZAIM_ACCESS_TOKEN_SECRET", ""))
+        consumer_key = os.environ.get("ZAIM_CONSUMER_KEY", "")
+        consumer_secret = os.environ.get("ZAIM_CONSUMER_SECRET", "")
+        access_token = os.environ.get("ZAIM_ACCESS_TOKEN", "")
+        access_token_secret = os.environ.get("ZAIM_ACCESS_TOKEN_SECRET", "")
         assert consumer_key, 'Please set "ZAIM_CONSUMER_KEY".'
         assert consumer_secret, 'Please set "ZAIM_CONSUMER_SECRET".'
         assert access_token, 'Please set "ZAIM_ACCESS_TOKEN".'
