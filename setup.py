847923caf52abeb0d6900aef3945ec0125415d6c
Hiromu <hiromu2000@hotmail.com>
2019-03-23 14:47:14 -0700

    0.2.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.2',
+    version='0.2.3',
 
     description='Python bindings for the Zaim API',
     long_description=readme + '\n\n' + history,
